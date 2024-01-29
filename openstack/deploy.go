a2ddab5819e39e1376be263aceaa21f32e4b5ebd
BradHacker <harkerbd@gmail.com>
2024-01-29 15:03:10 -0500

    Add extra fields to deploy resources

--- openstack/deploy.go
+++ openstack/deploy.go
@@ -78,9 +78,15 @@ func (provider ProviderOpenstack) Deploy(ctx context.Context, request *providerG
 				// HOST
 				case OpenstackResourceTypeHost:
 					// Add host to resource list
+					name := blueprint.Hosts[key].Hostname
+					if blueprint.Hosts[key].Name != nil {
+						name = *blueprint.Hosts[key].Name
+					}
 					response.Resources = append(response.Resources, &providerGRPC.DeployResource{
-						Key:  key,
-						Type: providerGRPC.ResourceType_HOST,
+						DeploymentId: request.DeploymentId,
+						Key:          key,
+						Name:         name,
+						Type:         providerGRPC.ResourceType_HOST,
 					})
 					// Deploy host
 					if err := provider.deployHost(ctx, authClient, request.DeploymentId, &varMap, &stateMap, blueprint, key); err != nil {
@@ -90,9 +96,15 @@ func (provider ProviderOpenstack) Deploy(ctx context.Context, request *providerG
 				// NETWORK
 				case OpenstackResourceTypeNetwork:
 					// Add network to resource list
+					name := key
+					if blueprint.Networks[key].Name != nil {
+						name = *blueprint.Networks[key].Name
+					}
 					response.Resources = append(response.Resources, &providerGRPC.DeployResource{
-						Key:  key,
-						Type: providerGRPC.ResourceType_NETWORK,
+						DeploymentId: request.DeploymentId,
+						Key:          key,
+						Name:         name,
+						Type:         providerGRPC.ResourceType_NETWORK,
 					})
 					// Deploy network
 					if err := provider.deployNetwork(ctx, authClient, request.DeploymentId, &varMap, &stateMap, blueprint, key); err != nil {
@@ -102,9 +114,15 @@ func (provider ProviderOpenstack) Deploy(ctx context.Context, request *providerG
 				// ROUTER
 				case OpenstackResourceTypeRouter:
 					// Add router to resource list
+					name := key
+					if blueprint.Routers[key].Name != nil {
+						name = *blueprint.Routers[key].Name
+					}
 					response.Resources = append(response.Resources, &providerGRPC.DeployResource{
-						Key:  key,
-						Type: providerGRPC.ResourceType_ROUTER,
+						DeploymentId: request.DeploymentId,
+						Key:          key,
+						Name:         name,
+						Type:         providerGRPC.ResourceType_ROUTER,
 					})
 					// Deploy router
 					if err := provider.deployRouter(ctx, authClient, request.DeploymentId, &varMap, &stateMap, blueprint, key); err != nil {
