0d9a8ad3d1b42276304e64131bdecf16c0e52abf
BradHacker <harkerbd@gmail.com>
2024-02-20 21:42:35 -0500

    Add check for data object in metadata

--- openstack/metadata.go
+++ openstack/metadata.go
@@ -26,6 +26,7 @@ func (provider ProviderOpenstack) ExtractResourceMetadata(ctx context.Context, r
 
 	// Initialize empty reply
 	reply := &pgrpc.ExtractResourceMetadataReply{
+		Success:  true,
 		Metadata: map[string]*pgrpc.Metadata{},
 	}
 
@@ -45,83 +46,76 @@ func (provider ProviderOpenstack) ExtractResourceMetadata(ctx context.Context, r
 			return extractResourceMetadataErrorReply("failed to marshal object for resource %s: %v", resource.Key, err), nil
 		}
 
-		// Check the object type
-		var t *OpenstackResourceType
+		// Only generate metadata for resource (not needed for data)
 		if object.Resource != nil {
-			t = object.Resource
-		} else if object.Data != nil {
-			t = object.Data
-		} else {
-			return extractResourceMetadataErrorReply("object needs either resource or data type"), nil
-		}
+			// Generate metadata based on type
+			switch *object.Resource {
+			// HOST
+			case OpenstackResourceTypeHost:
+				logrus.Debugf("Resource is type host")
 
-		// Generate metadata based on type
-		switch *t {
-		// HOST
-		case OpenstackResourceTypeHost:
-			logrus.Debugf("Resource is type host")
-
-			// Add all networks host is on as dependencies
-			for nk := range object.Host.Networks {
-				// Check the network exists in resources
-				if _, ok := resourceMap[nk]; !ok {
-					return extractResourceMetadataErrorReply("host %s depends on network %s which isn't defined", resource.Key, nk), nil
+				// Add all networks host is on as dependencies
+				for nk := range object.Host.Networks {
+					// Check the network exists in resources
+					if _, ok := resourceMap[nk]; !ok {
+						return extractResourceMetadataErrorReply("host %s depends on network %s which isn't defined", resource.Key, nk), nil
+					}
+					logrus.Debugf("\tAdding host dependency on network %s", nk)
+					reply.Metadata[resource.Key].DependsOnKeys = append(reply.Metadata[resource.Key].DependsOnKeys, nk)
 				}
-				logrus.Debugf("\tAdding host dependency on network %s", nk)
-				reply.Metadata[resource.Key].DependsOnKeys = append(reply.Metadata[resource.Key].DependsOnKeys, nk)
-			}
 
-			// Set host features
-			reply.Metadata[resource.Key].Features = &pgrpc.Features{
-				Power:   true,
-				Console: true,
-			}
-		// NETWORK
-		case OpenstackResourceTypeNetwork:
-			logrus.Debugf("Resource is type network")
-
-			// Set network features
-			reply.Metadata[resource.Key].Features = &pgrpc.Features{
-				Power:   false,
-				Console: false,
-			}
-		// ROUTER
-		case OpenstackResourceTypeRouter:
-			logrus.Debugf("Resource is type router")
-
-			// Add external network as dependency
-			if _, ok := resourceMap[object.Router.ExternalNetwork]; !ok {
-				return extractResourceMetadataErrorReply("router %s depends on external network %s which isn't defined", resource.Key, object.Router.ExternalNetwork), nil
-			}
-			logrus.Debugf("\tAdding router dependency on network %s", object.Router.ExternalNetwork)
-			reply.Metadata[resource.Key].DependsOnKeys = append(reply.Metadata[resource.Key].DependsOnKeys, object.Router.ExternalNetwork)
-
-			// Add all networks router is connected to as dependencies
-			for nk := range object.Router.Networks {
-				// Check the network exists in resources
-				if _, ok := resourceMap[nk]; !ok {
-					return extractResourceMetadataErrorReply("router %s depends on network %s which isn't defined", resource.Key, nk), nil
+				// Set host features
+				reply.Metadata[resource.Key].Features = &pgrpc.Features{
+					Power:   true,
+					Console: true,
 				}
-				logrus.Debugf("\tAdding router dependency on network %s", nk)
-				reply.Metadata[resource.Key].DependsOnKeys = append(reply.Metadata[resource.Key].DependsOnKeys, nk)
+			// NETWORK
+			case OpenstackResourceTypeNetwork:
+				logrus.Debugf("Resource is type network")
+
+				// Set network features
+				reply.Metadata[resource.Key].Features = &pgrpc.Features{
+					Power:   false,
+					Console: false,
+				}
+			// ROUTER
+			case OpenstackResourceTypeRouter:
+				logrus.Debugf("Resource is type router")
+
+				// Add external network as dependency
+				if _, ok := resourceMap[object.Router.ExternalNetwork]; !ok {
+					return extractResourceMetadataErrorReply("router %s depends on external network %s which isn't defined", resource.Key, object.Router.ExternalNetwork), nil
+				}
+				logrus.Debugf("\tAdding router dependency on network %s", object.Router.ExternalNetwork)
+				reply.Metadata[resource.Key].DependsOnKeys = append(reply.Metadata[resource.Key].DependsOnKeys, object.Router.ExternalNetwork)
+
+				// Add all networks router is connected to as dependencies
+				for nk := range object.Router.Networks {
+					// Check the network exists in resources
+					if _, ok := resourceMap[nk]; !ok {
+						return extractResourceMetadataErrorReply("router %s depends on network %s which isn't defined", resource.Key, nk), nil
+					}
+					logrus.Debugf("\tAdding router dependency on network %s", nk)
+					reply.Metadata[resource.Key].DependsOnKeys = append(reply.Metadata[resource.Key].DependsOnKeys, nk)
+				}
+
+				// Set router features
+				reply.Metadata[resource.Key].Features = &pgrpc.Features{
+					Power:   false,
+					Console: false,
+				}
 			}
 
-			// Set router features
-			reply.Metadata[resource.Key].Features = &pgrpc.Features{
-				Power:   false,
-				Console: false,
+			// Add dependencies based on depends_on
+			logrus.Debugf("Adding resource depends_on dependencies")
+			for _, dependsOnKey := range object.DependsOn {
+				// Check the network exists in resources
+				if _, ok := resourceMap[dependsOnKey]; !ok {
+					return extractResourceMetadataErrorReply("resource %s depends on resource %s which doesn't exist", resource.Key, dependsOnKey), nil
+				}
+				logrus.Debugf("\tAdding dependency %s", dependsOnKey)
+				reply.Metadata[resource.Key].DependsOnKeys = append(reply.Metadata[resource.Key].DependsOnKeys, dependsOnKey)
 			}
-		}
-
-		// Add dependencies based on depends_on
-		logrus.Debugf("Adding resource depends_on dependencies")
-		for _, dependsOnKey := range object.DependsOn {
-			// Check the network exists in resources
-			if _, ok := resourceMap[dependsOnKey]; !ok {
-				return extractResourceMetadataErrorReply("resource %s depends on resource %s which doesn't exist", resource.Key, dependsOnKey), nil
-			}
-			logrus.Debugf("\tAdding dependency %s", dependsOnKey)
-			reply.Metadata[resource.Key].DependsOnKeys = append(reply.Metadata[resource.Key].DependsOnKeys, dependsOnKey)
 		}
 	}
 
